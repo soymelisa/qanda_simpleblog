905846a2697d69bca3b9760f02f872433365795f
Melisa <soymelisa@yahoo.com>
2019-02-03 23:45:29 -0600

    added faker library

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import faker from 'faker'; 
+// import './index.css';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard'; 
 
+const App = () => {
+    return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar}/>
+        </a>
 
+        <div className="content">
+          <a href="/" className="author">Sam
+          </a>
+          <div className="metadata">
+            <span className="date">
+              Hoy a las 6:00pm
+            </span>
+          </div>
+          <div className="text">post equis de prueba :)</div>
+        </div>
+      </div>
+      </div>
+    );
+};
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(<App />, document.querySelector('#root'));
 serviceWorker.unregister();
